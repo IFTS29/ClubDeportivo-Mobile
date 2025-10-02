e0a0acc691513577a136a187dcb6803cad9f21c8
Mike <maflores86@gmail.com>
2025-10-01 23:31:54 -0300

    Refactor: Eliminar manejo manual de WindowInsets

--- app/src/main/java/com/example/pruebaprimeraclase/PagarCuotaMensualActivity.kt
+++ app/src/main/java/com/example/pruebaprimeraclase/PagarCuotaMensualActivity.kt
@@ -11,10 +11,6 @@ class PagarCuotaMensualActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_pagar_cuota_mensual)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
