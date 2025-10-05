75eea4e1b7e78193e38b87e447b14c0d494376d0
mariana <mariana.aiello89@gmail.com>
2025-10-05 16:56:12 -0300

    Unificacion de estilos de Registro Clientes y Listado Vencimientos

--- app/src/main/java/com/example/pruebaprimeraclase/ListarVencimientosDiaActivity.kt
+++ app/src/main/java/com/example/pruebaprimeraclase/ListarVencimientosDiaActivity.kt
@@ -8,8 +8,7 @@ import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class ListarVencimientosDiaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
