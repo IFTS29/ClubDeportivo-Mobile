2960fdf27e758de2a31ce13d29556453d5bd102e
mariana <mariana.aiello89@gmail.com>
2025-11-12 21:00:36 -0300

    creacion tablas users y persons

--- app/src/main/java/com/example/pruebaprimeraclase/DBHelper.kt
+++ app/src/main/java/com/example/pruebaprimeraclase/DBHelper.kt
@@ -1,4 +1,40 @@
 package com.example.pruebaprimeraclase
 
-class DBHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper(context: Context) : SQLiteOpenHelper(context, "ClubDeportivo.db", null, 1){
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(
+            "CREATE TABLE usuarios("+
+                    "userId INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                    "userName TEXT NOT NULL, " +
+                    "userRol TEXT NOT NULL," +
+                    "active INTEGER NOT NULL)"
+        )
+
+        db.execSQL(
+            "CREATE TABLE persons("+
+                    "personId INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "personName TEXT NOT NULL," +
+                    "personLastName TEXT NOT NULL," +
+                    "docType TEXT NOT NULL," +
+                    "docNumber TEXT NOT NULL," +
+                    "birthDate TEXT NOT NULL," +
+                    "address TEXT NOT NULL," +
+                    "email TEXT NOT NULL," +
+                    "phoneNumber TEXT NOT NULL)"
+        )
+
+        
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS productos")
+        onCreate(db)
+    }
+
+
 }
